0cc53b90bddf6fed3ab01c7103a7ac3e9f8df861
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-02-16 02:53:58 +0530

    Update settings.py

--- xerox/settings.py
+++ xerox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=hqx%b-sba$$v(r8((ffphg@$95id0d#@7jnivkxuoc!yjwnlg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','xeroxdetail.herokuapp.com']
 
